a7ee932e82c0dd169cd191ee4774e2b2f6992c81
julia <77528734+juliagronau@users.noreply.github.com>
2021-11-02 14:05:39 +0100

    Add to and delete from local storage

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
+//Get elements we want to manipulate / listen to
 const form = document.getElementById("task-form");
 const taskList = document.getElementById("task-list");
+//Get to dos from localStorage or set to empty array if there are none; JSON.parse() converts to array
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
+const getFromLocalStorage = () => {
+  todos.forEach((todo) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+    <input type="checkbox" name="mark-as-done"></input>
+    <p>${todo.name}</p>
+    <button class="btn btn-danger" name="delete">Delete</button>
+    <button class="btn btn-warning" name="edit">Edit</button>
+    `;
+    taskList.append(listItem);
+  });
+};
+getFromLocalStorage();
+
+//Event handler for sumbit event
 const handleSumbit = (event) => {
   event.preventDefault();
   const input = document.getElementById("input-field");
-  addNewTask(input.value);
+  if (input.value === "") {
+    alert("Please enter a to do");
+  } else {
+    addNewTask(input.value);
+  }
   input.value = "";
 };
 
+//Event handler for click events
 const handleClick = (event) => {
-  console.log(event.target);
   if (event.target.name === "delete") {
     deleteTask(event);
   } else if (event.target.name === "edit") {
@@ -19,37 +41,64 @@ const handleClick = (event) => {
   }
 };
 
+//Add a new to do to DOM
 const addNewTask = (task) => {
-  const list = document.getElementById("task-list");
   const listItem = document.createElement("li");
+  const todo = {
+    id: Date.now(),
+    name: task,
+  };
 
   listItem.innerHTML = `
     <input type="checkbox" name="mark-as-done"></input>
     <p>${task}</p>
-    <button class="btn btn-danger" name="delete">Löschen</button>
-    <button class="btn btn-warning" name="edit">Bearbeiten</button>
+    <button class="btn btn-danger" name="delete">Delete</button>
+    <button class="btn btn-warning" name="edit">Edit</button>
     `;
 
-  list.append(listItem);
+  taskList.append(listItem);
+  todos.push(todo);
+  addToLocalStorage(todo);
+};
+//Add new to do to localStorage
+const addToLocalStorage = () => {
+  //setItems with the key "todos", assigning it the value of the todos array after turning it into a string (can't store array in localStorage)
+  localStorage.setItem("todos", JSON.stringify(todos));
 };
 
+//Delete to do from DOM
 const deleteTask = (event) => {
-  console.log(event.target.parentNode);
   const task = event.target.parentNode;
+  deleteFromLocalStorage(todos, task);
   task.remove();
 };
 
+const deleteFromLocalStorage = (todos, task) => {
+  console.log(todos);
+  const findTaskIndex = todos.findIndex(
+    (todo) => todo.name == task.childNodes[3].innerHTML
+  );
+  todos.splice(findTaskIndex, 1);
+  addToLocalStorage();
+};
+
+//Edit to do in DOM
 const editTask = (event) => {
-  console.log(event.target.parentNode.childNodes[2].nextSibling);
   const input = event.target.parentNode.childNodes[2].nextSibling;
   input.contentEditable = true;
   input.focus();
 };
 
+//Mark to do as done in DOM
 const markAsDone = (event) => {
   const task = event.target.nextSibling.nextSibling;
-  task.style.textDecoration = "line-through red";
+  if (task.style.textDecoration === "line-through red") {
+    task.style.textDecoration = "none";
+  } else {
+    task.style.textDecoration = "line-through red";
+  }
 };
 
+//Event listeners
 form.addEventListener("submit", handleSumbit);
 taskList.addEventListener("click", handleClick);
